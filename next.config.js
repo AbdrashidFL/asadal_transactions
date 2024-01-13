a331896b876a7f3dedc9dddc06e4c5c886add14a
abdrashid <abdrashid.fl@gmail.com>
2024-01-13 15:40:04 +0600

    Рефакторинг

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_URL: process.env["API_URL "]
-    }
+	env: {
+		API_URL: process.env['API_URL '],
+	},
+	output: 'export',
 };
 
 module.exports = nextConfig;
